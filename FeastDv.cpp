2748535bb6f7b0d1fc93c4e6f97dc90c56f65947
Yeshwanth Bethi <yeshwanthravitheja@gmail.com>
2020-08-31 15:33:50 +1000

    The ROI calculation was wrong and has been corrected and now the frames show the featuremaps.

--- FeastDv.cpp
+++ FeastDv.cpp
@@ -10,6 +10,7 @@
 #include <eigen3/unsupported/Eigen/MatrixFunctions>
 #include <vector>
 #include <opencv2/core/eigen.hpp>
+#include <opencv2/imgproc.hpp>
 
 
 using namespace Eigen;
@@ -22,29 +23,23 @@ private:
 	float thresholdClose;
 	float thresholdOpen;
 	float eta;
-	float tau;
+	long tau;
 	int radius;
 	uint64_t  lastFrame = 0;
     uint64_t  displayFreq = 1e6;
 
 
-    int rows;
-	int cols;
+
 
 	MatrixXf w;
-//	Matrix<uint64_t,Dynamic,Dynamic> lastTime;
     MatrixXf thresholds;
     MatrixXf roiSurface;
     MatrixXf dotProduct;
-//    MatrixXf outputFrame;
-//    long long r;
+    MatrixXf outputFrame;
+    long long r;
 
-
-//	cv::Mat w;
-//	cv::Mat threshold;
 	dv::TimeMat lastFiringTimes;
-//	cv::Mat roiSurface;
-//    cv::Mat dotProduct;
+
 public:
 	static void initInputs(dv::InputDefinitionList &in) {
 		in.addEventInput("events");
@@ -52,7 +47,7 @@ public:
 
 	static void initOutputs(dv::OutputDefinitionList &out) {
 		out.addEventOutput("events");
-//		out.addFrameOutput("frames");
+		out.addFrameOutput("frames");
 	}
 
 	static const char *initDescription() {
@@ -61,22 +56,27 @@ public:
 
 	static void initConfigOptions(dv::RuntimeConfig &config) {
 		config.add("n_neurons",
-			dv::ConfigOption::intOption("Number of neurons", 9, 2, 100));
+			dv::ConfigOption::intOption("Number of neurons", 4, 2, 100));
         config.add("thresholdClose",
-                   dv::ConfigOption::floatOption("Threshold Close", 0.01, 0, 1));
+                   dv::ConfigOption::floatOption("Threshold Close", 0.001, 0, 1));
         config.add("thresholdOpen",
-                   dv::ConfigOption::floatOption("Threhsold Open", 0.01, 0, 1));
+                   dv::ConfigOption::floatOption("Threhsold Open", 0.0005, 0, 1));
         config.add("eta",
-                   dv::ConfigOption::floatOption("Eta", 0.001, 0, 1));
+                   dv::ConfigOption::floatOption("Eta", 0.005, 0, 1));
         config.add("tau",
-                   dv::ConfigOption::floatOption("Tau", 1000, 1, 2000));
-        config.add("roi_x",
-                   dv::ConfigOption::intOption("ROI X", 10, 2, 100));
-        config.add("roi_y",
-                   dv::ConfigOption::intOption("ROI Y", 10, 2, 100));
+                   dv::ConfigOption::longOption("Tau", 1 * 5e3, 4e3, 1e4));
+//        config.add("roi_x",
+//                   dv::ConfigOption::intOption("ROI X", 10, 2, 100));
+//        config.add("roi_y",
+//                   dv::ConfigOption::intOption("ROI Y", 10, 2, 100));
         config.add("radius",
-                   dv::ConfigOption::intOption("ROI radius", 4, 2, 100));
-
+                   dv::ConfigOption::intOption("ROI radius", 1, 1, 100));
+        config.add("displayFreq",
+                   dv::ConfigOption::longOption("display Frequency", 1e6, 5*1e4, 1e7));
+        config.setPriorityOptions({"n_neurons", "radius",
+                                   "displayFreq", "tau",
+                                   "eta","thresholdClose",
+                                   "thresholdOpen"});
 
 
 //        config.setPriorityOptions({"tau"});
@@ -84,9 +84,7 @@ public:
 
 	FeastDV() : n_neurons(9), thresholdClose(0.01), thresholdOpen(0.02), eta(0.01), tau(10000),radius(4),lastFiringTimes(inputs.getEventInput("events").size()) {
 		outputs.getEventOutput("events").setup(1,n_neurons,"Output feast events");
-//        cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE );
-        rows = inputs.getEventInput("events").sizeX();
-        cols = inputs.getEventInput("events").sizeY();
+
         w = MatrixXf::Random((2*radius+1)*(2*radius+1),n_neurons);
         w.colwise().normalize();
 
@@ -94,17 +92,19 @@ public:
         thresholds = MatrixXf::Zero(1, n_neurons);
         dotProduct = MatrixXf::Zero(1, n_neurons);
 
-//        double guess = sqrt(n_neurons);
-//        r = std::floor(guess);
-//        if(r*r == n_neurons){
-//            outputFrame = MatrixXf::Zero((2*radius+1)*r, (2*radius+1)*r);
-//        }
-//        else {
-//            r = std::ceil(guess);
-//            outputFrame = MatrixXf::Zero((2*radius+1)*r, (2*radius+1)*r);
-//        }
-//
-//        outputs.getFrameOutput("frames").setup((2*radius+1)*r,(2*radius+1)*r,"Output feast events");
+        double guess = sqrt(n_neurons);
+        r = std::floor(guess);
+        if(r*r == n_neurons){
+            outputFrame = Matrix<float, Dynamic,Dynamic>::Zero((2*radius+1)*r, (2*radius+1)*r);
+        }
+        else {
+            r = std::ceil(guess);
+            outputFrame = Matrix<float, Dynamic,Dynamic>::Zero((2*radius+1)*r, (2*radius+1)*r);
+        }
+
+//        dvModuleRegisterOutput(moduleData, "frames", "REMOVE");
+
+        outputs.getFrameOutput("frames").setup(650,650,"Output feast frames");
 //
 
 
@@ -119,7 +119,7 @@ public:
 	void run() override {
 		auto input  = inputs.getEventInput("events");
 		auto output = outputs.getEventOutput("events");
-//		auto frameOutput = outputs.getFrameOutput("frames");
+
 
         int event_x;
         int event_y;
@@ -131,16 +131,26 @@ public:
             lastFiringTimes.at(event.y(), event.x()) = event.timestamp();
             int winnerNeuron = -1;
             int maxDotProduct = 0;
-            if(event.x()-radius < 0 || event.x()+radius > rows -1 || event.y()-radius < 0 || event.y()+radius > cols-1){
+            if(event.x()-radius < 0 || event.x()+radius > input.sizeX() -1 || event.y()-radius < 0 || event.y()+radius > input.sizeY()-1){
                 continue;
             } else{
-
-                for(int x=event_x-radius;x<=event_x+radius;x++){
-                    for(int y=event_y-radius;y<=event_y+radius;y++){
-                       roiSurface(0,(x-event_x+radius)*radius + y-event_y+radius) = static_cast<float>(expf(
-                               -(static_cast<float>(event.timestamp() - lastFiringTimes.at(y,x))) / tau));
+                for(int x=0;x<2*radius+1;x++){
+                    for(int y=0;y<2*radius+1;y++){
+                        roiSurface(0,x*(2*radius+1) + y) = static_cast<float>(expf(
+                                -(static_cast<float>(event.timestamp() - lastFiringTimes.at(y+event_y-radius,x+event_x-radius))) / tau));
                     }
                 }
+//                IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+//                std::cout<<std::endl<<std::endl;
+
+//                for(int x=event_x-radius;x<=event_x+radius;x++){
+//                    for(int y=event_y-radius;y<=event_y+radius;y++){
+//                       roiSurface(0,(x-event_x+radius)*radius + y-event_y+radius) = static_cast<float>(expf(
+//                               -(static_cast<float>(event.timestamp() - lastFiringTimes.at(y,x))) / tau));
+//                    }
+//                }
+//                std::cout<<roiSurface.format(CleanFmt)<<std::endl;
+
                 roiSurface.normalize();
 
                 dotProduct = roiSurface*w;
@@ -153,10 +163,10 @@ public:
                 }
                 if (winnerNeuron > -1){
                     w.col(winnerNeuron) = w.col(winnerNeuron)*(1-eta) + roiSurface.transpose()*eta;
+//                    std::cout<<roiSurface.format(CleanFmt)<<std::endl;
                     w.col(winnerNeuron).normalize();
-//                    w_norm = cv::norm(w.col(winnerNeuron),cv::NormTypes::NORM_L2);
-//                    w.col(winnerNeuron) /= w_norm;
-//                    cv::normalize(w.col(winnerNeuron), w.col(winnerNeuron));
+//                    std::cout<<w.col(winnerNeuron).format(CleanFmt)<<std::endl;
+
                     thresholds(0,winnerNeuron) += thresholdClose;
                     output << dv::Event(event.timestamp(), 0,winnerNeuron,true);
                 }else{
@@ -164,68 +174,87 @@ public:
                 }
 
             }
-//            cv::Mat outFrame = cv::Mat((2*radius+1)*r, (2*radius+1)*r,CV_32F,cv::Scalar(0.0));
-//            cv::Mat featureMapCV;// = cv::Mat((2*radius+1), (2*radius+1),CV_32F,cv::Scalar(0.0));
-//
-//
-//            if (event.timestamp() - lastFrame > displayFreq) {
-//                for(int i = 0; i<n_neurons; i++){
-//                    Matrix<float, Dynamic, Dynamic> feature = Map<const Matrix<float, Dynamic,Dynamic>>(w.col(winnerNeuron).data(), 2*radius+1, 2*radius+1);
-//                    cv::eigen2cv(feature, featureMapCV);
-//                    featureMapCV.copyTo(outFrame(cv::Rect(static_cast<int>(i/r)*(2*radius+1),
-//                                                          static_cast<int>(i%r)*(2*radius+1),
-//                                                          static_cast<int>(2*radius+1),static_cast<int>(2*radius+1))));
-////                    outputFrame.block(static_cast<int>(i/r)*(2*radius+1),
-////                                      static_cast<int>(i%r)*(2*radius+1),
-////                                      static_cast<int>(2*radius+1),static_cast<int>(2*radius+1)) = Map<const Matrix<float, Dynamic,Dynamic>>(w.col(winnerNeuron).data(), 2*radius+1, 2*radius+1);
-//                }
-//
-////                cv::eigen2cv(outputFrame,outFrame);
-//
-//                frameOutput<<event.timestamp()<<outFrame<<dv::commit;
-//                lastFrame = event.timestamp();
-//
-//            }
+
+            if (event.timestamp() - lastFrame > displayFreq) {
+                auto frameOutput = outputs.getFrameOutput("frames");
+                auto outFrame = frameOutput.frame();
+
+                outFrame.setTimestamp(event.timestamp());
+                cv::Mat outFrameCV;// = cv::Mat::zeros(cv::Size((2*radius+1)*r,(2*radius+1)*r), CV_32F);
+                cv::Mat outFrameResizedCV;
+
+                for(int i = 0; i<n_neurons; i++){
+//                    cv::Mat feature;
+//                    MatrixXf featureEigen = Map<const Matrix<float, Dynamic,Dynamic>>(w.col(i).data(), 2*radius+1, 2*radius+1);
+//                    cv::eigen2cv(featureEigen,feature);
+////                    std::cout << "M = " << std::endl << " " << feature << std::endl << std::endl;
+//                    feature.copyTo(outFrameCV.colRange(static_cast<int>(i%r)*(2*radius+1),
+//                                                       static_cast<int>(2*radius+1+(i%r)*(2*radius+1))).rowRange(static_cast<int>(i/r)*(2*radius+1),static_cast<int>(2*radius+1+(i/r)*(2*radius+1)) ));
+
+                    outputFrame.block(static_cast<int>(i/r)*(2*radius+1),
+                                      static_cast<int>(i%r)*(2*radius+1),
+                                      static_cast<int>(2*radius+1),static_cast<int>(2*radius+1)) = Map<const Matrix<float, Dynamic,Dynamic>>(w.col(i).data(), 2*radius+1, 2*radius+1);
+
+                }
+
+                cv::eigen2cv(outputFrame,outFrameCV);
+                cv::resize(outFrameCV, outFrameResizedCV, cv::Size(650, 650), 0,0, cv::INTER_NEAREST);
+                cv::normalize(outFrameResizedCV, outFrameResizedCV, 0, 255, cv::NORM_MINMAX);
+                outFrameResizedCV.convertTo(outFrameResizedCV, CV_8UC1);
+                outFrame.setMat(outFrameResizedCV);
+                outFrame.commit();
+
+
+//                frameOutput<<event.timestamp()<<outFrameCV<<dv::commit;
+                lastFrame = event.timestamp();
+
+            }
 		}
         output << dv::commit;
 
 	}
 
 	void configUpdate() override {
-        n_neurons = config.getInt("n_neurons");
-        tau = config.getFloat("tau");
+        tau = config.getLong("tau");
         thresholdOpen = config.getFloat("thresholdOpen");
         thresholdClose= config.getFloat("thresholdClose");
-        radius = config.getInt("radius");
-        outputs.getEventOutput("events").setup(1,n_neurons,"Output feast events");
-        rows = inputs.getEventInput("events").sizeX();
-        cols = inputs.getEventInput("events").sizeY();
+        displayFreq = config.getLong("displayFreq");
 
-        w.resize(0,0);
-        thresholds.resize(0,0);
-        roiSurface.resize(0,0);
-        dotProduct.resize(0,0);
+        if(n_neurons!=config.getInt("n_neurons") || radius!=config.getInt("radius")){
+            n_neurons = config.getInt("n_neurons");
+            radius = config.getInt("radius");
+            w.resize(0,0);
+            thresholds.resize(0,0);
+            roiSurface.resize(0,0);
+            dotProduct.resize(0,0);
+            outputFrame.resize(0,0);
 
-        w = MatrixXf::Random((2*radius+1)*(2*radius+1),n_neurons);
-        w.colwise().normalize();
+            w = MatrixXf::Random((2*radius+1)*(2*radius+1),n_neurons);
+            w.colwise().normalize();
 
-        roiSurface = MatrixXf::Zero(1, (2*radius+1)*(2*radius+1));
-        thresholds = MatrixXf::Zero(1, n_neurons);
-        dotProduct = MatrixXf::Zero(1, n_neurons);
+            roiSurface = MatrixXf::Zero(1, (2*radius+1)*(2*radius+1));
+            thresholds = MatrixXf::Zero(1, n_neurons);
+            dotProduct = MatrixXf::Zero(1, n_neurons);
+            double guess = sqrt(n_neurons);
+            r = std::floor(guess);
+            if(r*r == n_neurons){
+                outputFrame = Matrix<float, Dynamic,Dynamic>::Zero((2*radius+1)*r, (2*radius+1)*r);
+            }
+            else {
+                r = std::ceil(guess);
+                outputFrame = Matrix<float, Dynamic,Dynamic>::Zero((2*radius+1)*r, (2*radius+1)*r);
+            }
+            outputs.getEventOutput("events").setup(1,n_neurons,"Output feast events");
 
-//        outputFrame.resize(0,0);
-//
-//        double guess = sqrt(n_neurons);
-//        r = std::floor(guess);
-//        if(r*r == n_neurons){
-//            outputFrame = MatrixXf::Zero((2*radius+1)*r, (2*radius+1)*r);
-//        }
-//        else {
-//            r = std::ceil(guess);
-//            outputFrame = MatrixXf::Zero((2*radius+1)*r, (2*radius+1)*r);
-//        }
-//        outputs.getFrameOutput("frames").setup((2*radius+1)*r,(2*radius+1)*r,"Output feast events");
-//
+
+
+        }
+
+
+
+
+
+
 
     }
 };
